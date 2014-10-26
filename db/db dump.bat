28b0ece1eb7287512c241a5ef76f58462995fa29
Frank <tlan16@sina.com>
2014-10-26 16:14:50 +1100

    db

--- db/db dump.bat
+++ db/db dump.bat
@@ -1,3 +1,3 @@
-del .\gpgproperty .sql
-C:\wamp\bin\mysql\mysql5.6.17\bin\mysqldump.exe -u root -proot gpgproperty  > .\gpgproperty .sql
+del .\gpgproperty.sql
+C:\wamp\bin\mysql\mysql5.6.17\bin\mysqldump.exe -u root -proot gpgproperty  > .\gpgproperty.sql
 Pause
